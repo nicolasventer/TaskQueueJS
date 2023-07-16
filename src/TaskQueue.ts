61658853a510dcf071a7c1ede93b6fb0903f0110
Nicolas VENTER <thedeal78@hotmail.fr>
2023-07-16 20:06:41 +0200

    fix queueMap

--- src/TaskQueue.ts
+++ src/TaskQueue.ts
@@ -18,8 +18,10 @@ export class TaskQueue {
 	public push<T>(task: () => Promise<T>, key = "") {
 		const promise = new Promise<T>((resolve, reject) => {
 			const queue = this.queueMap.get(key);
-			if (queue) queue.push(() => task().then(resolve).catch(reject));
-			else this.queueMap.set(key, [() => task().then(resolve).catch(reject)]);
+			if (queue) {
+				queue.push(() => task().then(resolve).catch(reject));
+				this.queueMap.set(key, queue);
+			} else this.queueMap.set(key, [() => task().then(resolve).catch(reject)]);
 			this.run(key);
 		});
 		return promise;
